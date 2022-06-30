e9cf3825f15a0ac0a2e93e332b1d3b15609fce5b
bibiw123 <bibiw123@yahoo.fr>
2022-06-30 12:36:45 +0100

    update db

--- assets/data/db.php
+++ assets/data/db.php
@@ -1,6 +1,68 @@
 <?php
 
 $websiteTitle = "A.F.R.I.C.A";
+$carousel = [
+    [
+        "image" => "assets/images/20ans de l'asso.jpg",
+        "alt" => "Une femme avec un livre à la main",
+        "title" => "Les 20 ANS de L'Association",
+        "isActive" => true
+    ],
+    [
+        "image" => "assets/images/Africa 8 mars 2007 journée de la femme.jpg",
+        "alt" => "Une femme qui rigole avec une serviette bleu sur la tête",
+        "title" => "Journée de la femme,le 8 Mars 2007",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/Atelier enfants.jpeg",
+        "alt" => "Des enfants montrant des dessins",
+        "title" => "Atelier enfant",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/Cours informatique.jpeg",
+        "alt" => "Un homme donne des cours d'informatique à des femmes",
+        "title" => "Cours d'informatique",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/Evenement.JPG",
+        "alt" => "Une femme qui chante",
+        "title" => "Evénement",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/Manifestation régularisation.jpeg",
+        "alt" => "Manifestation régularisation",
+        "title" => "Manifestation pour la régularisation",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/repas solidaire.jpeg",
+        "alt" => "repas solidaire autour d'un repas",
+        "title" => "Repas solidaire",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/mur_local1.jpeg",
+        "alt" => "affiche mural local AFRICA",
+        "title" => "Affiches mural AFRICA",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/mur_local2.jpeg",
+        "alt" => "affiche mural local AFRICA",
+        "title" => "Affiches mural AFRICA",
+        "isActive" => false
+    ],
+    [
+        "image" => "assets/images/mur_local3.jpeg",
+        "alt" => "affiche mural local AFRICA",
+        "title" => "Affiches mural AFRICA",
+        "isActive" => false
+    ],
+];
 $actions = [
     [
         "image" => "assets/images/café_féministe.png",
